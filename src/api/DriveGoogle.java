112d2716ddfec1ddff49c540dd00c1b22f3e8ecd
HugoPoi <hugo.poissonnet@gmail.com>
2013-06-23 18:29:59 +0200

    Add google drive api

---
+++ src/api/DriveGoogle.java
@@ -0,0 +1,111 @@
+package api;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.json.simple.JSONObject;
+
+public class DriveGoogle implements IntDrive {
+	
+	private static String app_key,app_secret,app_redirect_uri;
+	
+	public String authUrl;
+	
+	private GoogleAuthorizationCodeFlow flow;
+	private HttpTransport httpTransport;
+	private JsonFactory jsonFactory;
+	private Drive service;
+	
+	public static void init(String key, String secret, String redirect_uri){
+		app_key = key;
+		app_secret = secret;
+		app_redirect_uri = redirect_uri;
+	}
+	
+	public DriveGoogle() {
+		httpTransport = new NetHttpTransport();
+	    jsonFactory = new JacksonFactory();
+	   
+	    flow = new GoogleAuthorizationCodeFlow.Builder(
+	        httpTransport, jsonFactory, app_key, app_secret, Arrays.asList(DriveScopes.DRIVE))
+	        .setAccessType("online")
+	        .setApprovalPrompt("auto").build();
+	    
+	    authUrl = flow.newAuthorizationUrl().setRedirectUri(app_redirect_uri).build();
+
+	}
+	
+	public Boolean validateToken(String code){
+	    GoogleTokenResponse response;
+		try {
+			response = flow.newTokenRequest(code).setRedirectUri(app_redirect_uri).execute();
+			GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
+			//Create a new authorized API client
+		    service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	@Override
+	public void uploadFile(String path, OutputStream file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Entry getEntryInfo(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Entry> getEntries(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addDrive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeDrive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IntDrive listDrive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JSONObject savedState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
